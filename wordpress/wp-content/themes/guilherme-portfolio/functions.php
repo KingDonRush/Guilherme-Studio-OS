02ce489875612914e5840cb85cd49c9d2926789b
KingDonRush <guilherme.manoelbbs@gmail.com>
2026-06-16 02:09:55 -0300

    Remove Simple Budget contact icon backgrounds

--- wordpress/wp-content/themes/guilherme-portfolio/functions.php
+++ wordpress/wp-content/themes/guilherme-portfolio/functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'GP_THEME_VERSION', '0.3.30' );
+define( 'GP_THEME_VERSION', '0.3.31' );
 define( 'GP_THEME_DIR', get_template_directory() );
 define( 'GP_THEME_URI', get_template_directory_uri() );
 
