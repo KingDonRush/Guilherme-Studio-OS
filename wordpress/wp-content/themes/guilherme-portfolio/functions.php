d879b5d12f75a18c13078060e9ec278d0bdd6d8e
KingDonRush <guilherme.manoelbbs@gmail.com>
2026-06-16 00:50:03 -0300

    Balance Simple Budget carousel spacing

--- wordpress/wp-content/themes/guilherme-portfolio/functions.php
+++ wordpress/wp-content/themes/guilherme-portfolio/functions.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'GP_THEME_VERSION', '0.3.25' );
+define( 'GP_THEME_VERSION', '0.3.26' );
 define( 'GP_THEME_DIR', get_template_directory() );
 define( 'GP_THEME_URI', get_template_directory_uri() );
 
