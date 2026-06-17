bab675932c003f10db3757ead85e46b52076e3cb
KingDonRush <guilherme.manoelbbs@gmail.com>
2026-06-17 17:03:14 -0300

    fix: decode local api panel path

--- packages/local-api/src/bin.ts
+++ packages/local-api/src/bin.ts
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
+import { fileURLToPath } from "node:url";
 import { serveLocalApi } from "./index.js";
 
-const panelDist = new URL("../../../apps/panel/dist", import.meta.url).pathname;
+const panelDist = fileURLToPath(new URL("../../../apps/panel/dist", import.meta.url));
 const { token, url } = await serveLocalApi({ panelDist });
 
 console.log(JSON.stringify({ url, token }, null, 2));
